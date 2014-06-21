2348b8a3886f3e689f2dfaf5cd46474f97be135a
Dan(n) Blair <dann@boldapps.net>
2014-06-20 20:17:53 -0500

    Game is not functional. Getting ready to push to the store

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html',
-    {minWidth: 630, minHeight: 650,
-    maxWidth: 630, maxHeight: 650});
+    {minWidth: 350, minHeight: 530,
+    maxWidth: 350, maxHeight: 530});
 });
